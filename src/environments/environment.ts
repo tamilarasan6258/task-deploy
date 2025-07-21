55825fac09ed2f7e1164b4954c2197a4da922e42
tamilarasan6258 <sstamilarasan14@gmail.com>
2025-07-21 22:32:53 +0530

    for deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  auth_apiBaseUrl: 'http://localhost:5000/api/auth',
+  password_apiBaseUrl: 'http://localhost:5000/api/password',
+  project_apiBaseUrl: 'http://localhost:5000/api/projects',
+  task_apiBaseUrl: 'http://localhost:5000/api/tasks'
+};
+
+// export const environment = {
+//   production: false,
+//   auth_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/auth',
+//   password_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/password',
+//   project_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/projects',
+//   task_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/tasks'
+// };
