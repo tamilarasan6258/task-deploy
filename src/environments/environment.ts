1456be7de410671422426bef461c788d50fb1eb4
tamilarasan6258 <sstamilarasan14@gmail.com>
2025-07-23 00:44:28 +0530

    fix environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
-// export const environment = {
-//   production: false,
-//   auth_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/auth',
-//   password_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/password',
-//   project_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/projects',
-//   task_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/tasks'
-// };
-
 export const environment = {
   production: false,
-  auth_apiBaseUrl: 'http://localhost:5000/api/auth',
-  password_apiBaseUrl: 'http://localhost:5000/api/password',
-  project_apiBaseUrl: 'http://localhost:5000/api/projects',
-  task_apiBaseUrl: 'http://localhost:5000/api/tasks'
+  auth_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/auth',
+  password_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/password',
+  project_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/projects',
+  task_apiBaseUrl: 'https://task-deploy-1-yw4f.onrender.com/api/tasks'
 };
+
+// export const environment = {
+//   production: false,
+//   auth_apiBaseUrl: 'http://localhost:5000/api/auth',
+//   password_apiBaseUrl: 'http://localhost:5000/api/password',
+//   project_apiBaseUrl: 'http://localhost:5000/api/projects',
+//   task_apiBaseUrl: 'http://localhost:5000/api/tasks'
+// };
 
 // export const environment = {
 //   production: false,
